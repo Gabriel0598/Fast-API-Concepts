45fe4395d41d28876cb8b35570fba903aba568c1
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-04-05 22:35:34 -0300

    Update info on setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-name='mypackage',
-version='0.1',
-author='Your Name',
-author_email='your.email@example.com',
-description='A description of your package',
+name='fast_api_packages',
+version='1.0',
+author='Gabriel Augusto Ferreira',
+author_email='REDACTED',
+description='Packages for Fast API Project',
 long_description=open('README.md').read(),
 long_description_content_type='text/markdown',
-url='https://github.com/yourusername/mypackage',
+url='https://github.com/Gabriel0598/Fast-API-Concepts',
 packages=find_packages(),
 install_requires=[
 'pytest>=8.3.5',
@@ -19,5 +19,5 @@ classifiers=[
 'License :: OSI Approved :: MIT License',
 'Operating System :: OS Independent',
 ],
-python_requires='>=3.6',
+python_requires='>=3.11',
 )
