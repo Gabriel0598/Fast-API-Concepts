82d8a22bc6877916a5018c1115812fb4e1b7df53
Gabriel Augusto Ferreira <gabrielaugustoferreira@outlook.com>
2025-04-05 22:30:47 -0300

    README/ Config setup and requirements

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+name='mypackage',
+version='0.1',
+author='Your Name',
+author_email='your.email@example.com',
+description='A description of your package',
+long_description=open('README.md').read(),
+long_description_content_type='text/markdown',
+url='https://github.com/yourusername/mypackage',
+packages=find_packages(),
+install_requires=[
+'pytest>=8.3.5',
+'setuptools>=78.1.0'
+],
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
